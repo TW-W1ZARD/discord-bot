4b596d35ac66581a474d3628589f6d04df61f680
TW-W1ZARD <81258557+TW-W1ZARD@users.noreply.github.com>
2021-11-21 13:45:25 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const fivereborn = require('fivereborn-query');
+bot.commands = new Discord.Collection();
+
+const token = 'ODc2MDg1MjM4MTMwOTUwMTg0.YRe79Q.1nEBuppwf_Nts_3WtnLdvU-yF6A'; // 🎈 Here put the Token of your bot
+
+
+
+bot.on('ready', () => {
+    console.log(`    `);
+    console.log(`    `);
+    console.log(`    `);
+    console.log(`    `);
+    console.log(`    `);
+    console.log(`    `);
+    console.log(`    `);
+    console.log(`    `);
+    console.log(`    `);
+    console.log('                          888                        888                       ');
+    console.log('                          888                        888                       ');
+    console.log('                          888                        888                       ');
+    console.log('                          88888b.   .d88b.   .d8888b 888  888  .d88b.  888d888 ');
+    console.log('                          888 "88b d8P  Y8b d88P"    888 .88P d8P  Y8b 888P"   ');
+    console.log('                          888  888 88888888 888      888888K  88888888 888     ');
+    console.log('                          888  888 Y8b.     Y88b.    888 "88b Y8b.     888     ');
+    console.log('                          888  888  "Y8888   "Y8888P 888  888  "Y8888  888     ');
+    console.log(`             `);
+    console.log(`                   Creator : TW_W1ZARD#6977 | Version : 1.0.5 Bêta | Bot : ${bot.user.username}`);
+});
+
+
+
+function activity(){
+    setTimeout(() => {
+        fivereborn.query("176.9.67.34","30330", (err, data) => { // replace the 'n°1' by your server IP and replace the 'n°2' by the Port of your server
+            if (err) {
+                console.log(err);
+            } else {
+                bot.user.setStatus('available') // you can change this for : available, idle, or invisible. "if you want this to appear on the bot profile consider removing the stream function"
+                bot.user.setPresence({
+                    game: {
+                        name: `${data.clients}/${data.maxclients} joueur(s)`, // you can change the joueur for players
+                        type: "STREAMING",
+                        url: "https://www.twitch.tv/tw_w1zard" // you can change the "alive-korp" by a other tag
+                    }
+                });
+
+            }
+        });
+        activity(); 
+    }, 1000); // you can change the refresh time (ms)
+}
+activity();
+
+bot.login(process.env.TOKEN)
